f5db78be1278d2851c3bb73ccb206929a0bae710
20032049-ChengXinLin <87117809+20032049-ChengXinLin@users.noreply.github.com>
2022-05-26 22:01:22 +0800

    Update PlayerRepository.java

--- TicTaeToe/src/main/java/Xinlin/TicTaeToe/PlayerRepository.java
+++ TicTaeToe/src/main/java/Xinlin/TicTaeToe/PlayerRepository.java
@@ -4,10 +4,7 @@
 * I will not copy or allow others to copy my code.
 * I understand that copying code is considered as plagiarism.
 *
-* Student Name: Cheng Xin Lin (20032049), Koh Siew Gek (20008303), Chen Wan Ting (20009334)
-* Team ID: SOI-2022-0012
-* Team Project ID: SOI-2022-2210-0049
-* Project: [IP] Digital Wallet
+* Student Name: Cheng Xin Lin (20032049)
 * Date created: 2022-May-25 8:55:34 pm
 *
 */
